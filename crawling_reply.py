d23d689056f0507c0b05525e209266c7cca3ba05
jseongnam <105405028+jseongnam@users.noreply.github.com>
2022-11-29 11:48:13 +0900

    crawling_reply 수정

--- crawling_reply.py
+++ crawling_reply.py
@@ -5,7 +5,8 @@ import re
 import time
 import datetime
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 category = []
 pages = []
 
@@ -13,6 +14,7 @@ pages = []
 options = webdriver.ChromeOptions()
 # options.add_argument('headless')
 options.add_argument('lang=kr_KR')
+options.add_argument("--disable-popup-blocking")
 driver = webdriver.Chrome('./chromedriver', options=options)
 df_reply = pd.DataFrame()
 replys = []
@@ -20,19 +22,40 @@ replys = []
 for i in range(1, 2):  # page_count
     url = 'https://www.ssg.com/disp/category.ssg?ctgId=6000092898&page={}'.format(i) #우유 url
     driver.get(url)
-    for j in range(1, 2): # product_count
+    for j in range(1, 81): # product_count
         #product 클릭
         try:
             time.sleep(0.5)
             btn_product = '//*[@id="ty_thmb_view"]/ul/li[{}]/div[2]/div[2]/div/a'.format(j) #제품 xpath
             clicked_btn_product = driver.find_element('xpath', btn_product).send_keys(Keys.ENTER)
 
+            try : # 에러창이 나올 시, 3초 기다리고 에러창 끈 후에 이전 URL로 이동
+                WebDriverWait(driver, 3).until(EC.alert_is_present())
+                alert = driver.switch_to.alert
+                alert.accept()
+                time.sleep(1)
+                driver.get(url)
+                pass
+
+            except: # 에러창 없을 시, 코드 계속 진행
+                continue
         except NoSuchElementException as e:
             try:
                 time.sleep(0.5)
                 btn_product = '//*[@id="ty_thmb_view"]/ul/li[{}]/div[2]/div[2]/div/a/em[1]'.format(j)   #제품 xpath
                 clicked_btn_product = driver.find_element('xpath', btn_product).send_keys(Keys.ENTER)
                 time.sleep(1)
+
+                try:  # 에러창이 나올 시, 3초 기다리고 에러창 끈 후에 이전 URL로 이동
+                    WebDriverWait(driver, 3).until(EC.alert_is_present())
+                    alert = driver.switch_to.alert
+                    alert.accept()
+                    time.sleep(1)
+                    driver.get(url)
+                    pass
+
+                except:  # 에러창 없을 시, 코드 계속 진행
+                    continue
             except:
                 print('none')
 
@@ -48,7 +71,7 @@ for i in range(1, 2):  # page_count
                         print(reply)
                         reply = re.compile('[^가-힣]').sub(' ', reply)
                         replys.append(reply)
-
+                        time.sleep(0.5)
                     except NoSuchElementException as e:
                         print('none')
             except StaleElementReferenceException as r:
@@ -60,13 +83,13 @@ for i in range(1, 2):  # page_count
         driver.back()
         time.sleep(3)
 
-if i % 1 == 0:
-    df_section_reply = pd.DataFrame(replys, columns=['reply'])
-    df_section_reply['category'] = 'Dairy'
-    df_title = pd.concat([df_reply, df_section_reply], ignore_index=True)
-    df_title.to_csv('./crawling_data/crawling_data_{}_To_{}.csv'.format(i, j),
-                            index = False)
-    replys = []
+    if i % 1 == 0:  #1번째마다 저장
+        df_section_reply = pd.DataFrame(replys, columns=['reply'])
+        df_section_reply['category'] = 'Dairy'
+        df_title = pd.concat([df_reply, df_section_reply], ignore_index=True)
+        df_title.to_csv('./crawling_data/crawling_data_{}_To_{}.csv'.format(i, j),
+                                index = False)
+        replys = []
 
 
 
