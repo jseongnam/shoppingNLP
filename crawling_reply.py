44ace46fac2f21f03232c30fff0e4212c56f6180
jseongnam <105405028+jseongnam@users.noreply.github.com>
2022-11-29 11:25:24 +0900

    주석작업

--- crawling_reply.py
+++ crawling_reply.py
@@ -18,18 +18,19 @@ df_reply = pd.DataFrame()
 replys = []
 
 for i in range(1, 2):  # page_count
-    url = 'https://www.ssg.com/disp/category.ssg?ctgId=6000092898&page={}'.format(i)
+    url = 'https://www.ssg.com/disp/category.ssg?ctgId=6000092898&page={}'.format(i) #우유 url
     driver.get(url)
     for j in range(1, 2): # product_count
+        #product 클릭
         try:
             time.sleep(0.5)
-            btn_product = '//*[@id="ty_thmb_view"]/ul/li[{}]/div[2]/div[2]/div/a'.format(j)
+            btn_product = '//*[@id="ty_thmb_view"]/ul/li[{}]/div[2]/div[2]/div/a'.format(j) #제품 xpath
             clicked_btn_product = driver.find_element('xpath', btn_product).send_keys(Keys.ENTER)
 
         except NoSuchElementException as e:
             try:
                 time.sleep(0.5)
-                btn_product = '//*[@id="ty_thmb_view"]/ul/li[{}]/div[2]/div[2]/div/a/em[1]'.format(j)
+                btn_product = '//*[@id="ty_thmb_view"]/ul/li[{}]/div[2]/div[2]/div/a/em[1]'.format(j)   #제품 xpath
                 clicked_btn_product = driver.find_element('xpath', btn_product).send_keys(Keys.ENTER)
                 time.sleep(1)
             except:
@@ -38,18 +39,15 @@ for i in range(1, 2):  # page_count
         for k in range (1, 10):
             try:
                 time.sleep(0.5)
-                btn_reply = '//*[@id="comment_navi_area"]/a[{}]'.format(k)
+                btn_reply = '//*[@id="comment_navi_area"]/a[{}]'.format(k)  #k번째 제품들 xpath
                 clicked_btn_reply = driver.find_element('xpath', btn_reply).send_keys(Keys.ENTER)
                 for l in range(1, 20, 2):
                     try:
-                        reply = '//*[@id="cdtl_cmt_tbody"]/tr[{}]/td[1]/div/a/div[1]/span'.format(l)
-                        print('none')
+                        reply = '//*[@id="cdtl_cmt_tbody"]/tr[{}]/td[1]/div/a/div[1]/span'.format(l)    #댓글 xpath
                         reply = driver.find_element('xpath', reply).text
                         print(reply)
                         reply = re.compile('[^가-힣]').sub(' ', reply)
-                        print('none')
                         replys.append(reply)
-                        print('none')
 
                     except NoSuchElementException as e:
                         print('none')
