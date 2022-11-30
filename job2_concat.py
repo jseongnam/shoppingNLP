0c166f46eac3dccb3dca4e59f901960428d5e42f
jseongnam <105405028+jseongnam@users.noreply.github.com>
2022-11-30 10:07:57 +0900

    concat 코드 푸시입니다. 10:07

--- job2_concat.py
+++ job2_concat.py
@@ -5,7 +5,7 @@ import datetime
 data_path = glob.glob('./crawling_data/*.csv')
 print(data_path)
 df = pd.DataFrame()
-for path in data_path[:-4]:
+for path in data_path:
     df_temp = pd.read_csv(path)
     df = pd.concat([df, df_temp], ignore_index=True)
 df.drop_duplicates(inplace=True)
@@ -15,5 +15,4 @@ df.dropna(inplace=True)
 df.reset_index(inplace=True, drop=True)
 print(df.head())
 df.info()
-df.to_csv('./crawling_data/naver_news_titles_{}.csv'.format(
-    datetime.datetime.now().strftime('%Y%m%d')), index=False)
+df.to_csv('./crawling_data/crawling_data_animal.csv', index=False)
