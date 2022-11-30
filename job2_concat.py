7e624a1197cfe54d9950873a67d8dfdcfcd8312d
jseongnam <105405028+jseongnam@users.noreply.github.com>
2022-11-30 18:14:01 +0900

    code 수정입니다. 6:13

--- job2_concat.py
+++ job2_concat.py
@@ -2,15 +2,12 @@ import pandas as pd
 import glob
 
 data_path = glob.glob('./crawling_data/*.csv')
-print(data_path)
 df = pd.DataFrame()
 for path in data_path:
     df_temp = pd.read_csv(path)
     df = pd.concat([df, df_temp], ignore_index=True)
 df.drop_duplicates(inplace=True)
-print(df.head(30))
 df.dropna(inplace=True)
 df.reset_index(inplace=True, drop=True)
-print(df.head())
 df.info()
 df.to_csv('./crawling_data_2/crawling_data_concat_1.csv', index=False)
